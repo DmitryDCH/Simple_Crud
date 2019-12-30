92a41b895a93892bfe4d213eb115e6f40c62b948
Dmitry Chekurkov <chekurkov@gmail.com>
2019-12-30 17:24:29 +0200

    Create basic Client skeleton

---
+++ Client/src/components/containers/App/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return(
+      <p>Hello from App</p>
+    );
+  }
+}
+
+export default App;
